9d25e1fdc0e50b890aca8431514588bba4d17112
Rich Bakos <rbakos@gmail.com>
2024-07-08 22:24:11 +0000

    Reformatting resources

--- terraform/prowlarr/resources.tf
+++ terraform/prowlarr/resources.tf
@@ -31,17 +31,17 @@ resource "prowlarr_host" "prowlarr" {
 }
 
 resource "prowlarr_application_radarr" "radarr" {
-  name            = "Radarr"
-  sync_level      = "fullSync"
-  base_url        = "http://radarr.service.consul:7878"
-  prowlarr_url    = "http://prowlarr.service.consul:9696"
-  api_key         = "167390ac0e444bb885a1b1e2600d9886"
+  name					= "Radarr"
+  sync_level		= "fullSync"
+  base_url			= "http://radarr.service.consul:7878"
+  prowlarr_url	= "http://prowlarr.service.consul:9696"
+  api_key				= "167390ac0e444bb885a1b1e2600d9886"
 }
 
 resource "prowlarr_application_sonarr" "sonarr" {
-  name                  = "Sonarr"
-  sync_level            = "fullSync"
-  base_url              = "http://sonarr.service.consul:8989"
-  prowlarr_url          = "http://prowlarr.service.consul:9696"
-  api_key               = "5ee2e2925e284729b034f973f1141458"
+  name					= "Sonarr"
+  sync_level		= "fullSync"
+  base_url			= "http://sonarr.service.consul:8989"
+  prowlarr_url	= "http://prowlarr.service.consul:9696"
+  api_key				= "5ee2e2925e284729b034f973f1141458"
 }
--- terraform/sonarr/resources.tf
+++ terraform/sonarr/resources.tf
@@ -31,12 +31,12 @@ resource "sonarr_host" "sonarr" {
 }
 
 resource "sonarr_download_client_transmission" "example" {
-  enable   									 = true
-  priority 									 = 1
-  name     									 = "Transmission"
-  host     									 = "transmission.service.consul"
-  url_base                   = "/transmission/" 
-  port     									 = 9091
+  enable    = true
+  priority  = 1
+  name      = "Transmission"
+  host      = "transmission.service.consul"
+  url_base  = "/transmission/" 
+  port      = 9091
 }
 
 # data "sonarr_remote_path_mapping" "mapping" {
