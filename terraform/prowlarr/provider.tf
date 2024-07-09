f185da141add4b7764b1a226a2b30c5a49335ed5
Rich Bakos <rbakos@gmail.com>
2024-07-09 03:13:15 +0000

    Added providers - Radarr, Sonarr

--- terraform/prowlarr/provider.tf
+++ terraform/prowlarr/provider.tf
@@ -8,18 +8,6 @@ terraform {
 }
 
 provider "prowlarr" {
-  url = var.url
-  api_key = var.api_key
-}terraform {
-  required_providers {
-    prowlarr = {
-      source = "devopsarr/prowlarr"
-      version = "2.4.0"
-    }
-  }
-}
-
-provider "prowlarr" {
   url     = var.url
   api_key = var.api_key
 }
